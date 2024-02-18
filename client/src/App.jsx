1141ffedd51a2bcb3d7b9ca1523a76e6f50f2993
vinish1997 <vinish.choudhary.1997@gmail.com>
2024-02-18 11:19:49 +0530

    Create new files for project and added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
 
 export default function APP() {
   return (
-    <div className='text-3xl text-red-500'>APP</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/projects' element={<Projects/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-out' element={<SignOut/>} />
+      </Routes>
+    </BrowserRouter>
+    )
 }
