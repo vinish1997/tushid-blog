52c662c8466b2ef84ac78f3e89ee0643ea426a05
vinish1997 <vinish.choudhary.1997@gmail.com>
2024-02-18 21:29:14 +0530

    feat: Add new header and integrate Flowbite library for ready-made components.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import About from './pages/About';
 import Projects from './pages/Projects';
 import SignIn from './pages/SignIn';
 import SignOut from './pages/SignOut';
+import Header from './components/Header';
 
 export default function APP() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/dashboard' element={<Dashboard/>} />
