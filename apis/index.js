d1ecbba856f99796404940944050243e22fe9623
Vinish Choudhary <Vinish.choudhary.1997@gmail.com>
2024-02-24 09:01:18 +0530

    feat: connect to database using mongo connection string

--- apis/index.js
+++ apis/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_CONNECTION_STRING).then(()=>{
+    console.log("mongo connected");
+})
 
 const app = express();
 
 app.listen(3000, ()=>{
-    console.log("welcome to first express server!!!");
+    console.log("welcome to first express server!!");
 });
