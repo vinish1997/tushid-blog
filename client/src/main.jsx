fed538bd1ba024ca67b619bd860112cef560410d
Vinish Choudhary <Vinish.choudhary.1997@gmail.com>
2024-03-02 01:15:57 +0530

    feat: added dark anf ligth mode theme with button on header.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,16 @@ import "./index.css";
 import { persistor, store } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import ThemeProvider from "./components/ThemeProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <PersistGate persistor={persistor}>
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <Provider store={store}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </Provider>
     </PersistGate>
   </React.StrictMode>
 );
