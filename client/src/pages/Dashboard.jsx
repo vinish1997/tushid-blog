ccaa2f957b9044d3c6f401d6058f1952d435e83f
Vinish Choudhary <Vinish.choudhary.1997@gmail.com>
2024-03-02 19:57:24 +0530

    feat: complete sidebar ui for dashboard.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashboardSidebar from "../components/DashboardSidebar";
+import DashboardProfile from "../components/DashboardProfile";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParam = new URLSearchParams(location.search);
+    const tabFromUrl = urlParam.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      {/* Side Bar */}
+      <div className="md:w-56">
+        <DashboardSidebar />
+      </div>
+      {/* Profile */}
+      <div className="">{tab === "profile" && <DashboardProfile />}</div>
+    </div>
+  );
 }
