63b482bc9ec3cef6ece41f1c44874f5ae2ef6615
Vinish Choudhary <Vinish.choudhary.1997@gmail.com>
2024-03-18 10:26:27 +0530

    feat: created a api route to fetch all comments for a post.

--- apis/routes/comment.route.js
+++ apis/routes/comment.route.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyToken.js";
-import { createComment } from "../controllers/comment.controller.js";
+import {
+  createComment,
+  getPostComments,
+} from "../controllers/comment.controller.js";
 
 const commentRoute = express.Router();
 
 commentRoute.post("", verifyToken, createComment);
+
+commentRoute.get("/post/:postId", getPostComments);
 
 export default commentRoute;
